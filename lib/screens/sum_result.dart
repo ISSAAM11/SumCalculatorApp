01ef38bfb3232ba8d19198dc197bd436237fb8b0
ISSAAM11 <benhassineissam@gmail.com>
2024-08-09 17:53:14 +0100

    issam stash commit

--- lib/screens/sum_result.dart
+++ lib/screens/sum_result.dart
@@ -51,6 +51,7 @@ class _SumResultState extends State<SumResult> {
                 productsMap: widget.sumItem.productsMap),
             // new git push
             // this is new change of issam branch
+            // this a second stash test to stwitch between stash test
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
